fa67396f65347fef43df6b502ef38f99cfb554a9
Yyax13 <Yyax13@Proton.me>
2025-04-21 10:59:35 -0300

    Created accessLevelMiddleware that check if user have accesslevel for doing smt

---
+++ middlewares/accessLevelMiddleware.js
@@ -0,0 +1,15 @@
+import genResult from "../results";
+import redirectToStatusPage from "./httpStatusMiddleware";
+
+function checkAccessLevel(requiredLevel) {
+    const result = genResult;
+    return (req, res, next) => {
+        if (req.session?.cookie4set.AcessLevel <= requiredLevel) {
+            next();
+        } else {
+            redirectToStatusPage(404);
+        }
+    }
+}
+
+export default checkAccessLevel;
